055e543f0523ab392334d9961e092bc6d009a236
Skysparko <shubhamrakhecha5@gmail.com>
2024-12-07 00:10:17 +0530

    made check slot public

--- src/modules/trainee/traineeRoute.ts
+++ src/modules/trainee/traineeRoute.ts
@@ -17,7 +17,7 @@ const traineeMiddleware = new TraineeMiddleware();
 
 route.use([
   (req, res, next) => {
-    req.byPassRoute = ["/get-trainers-with-slots"];
+    req.byPassRoute = ["/get-trainers-with-slots","/check-slot"];
     next();
   },
   authorizeMiddleware.authorizeUser,
